29bb3db6e12cbfd143ba143012f713d8addc1db7
JEEVA BHARATHI <JeevabharathiRK@gmail.com>
2023-07-19 13:30:28 +0530

    Clean interface

--- Credit_Bin/app/src/main/java/in/co/learn/MainActivity.kt
+++ Credit_Bin/app/src/main/java/in/co/learn/MainActivity.kt
@@ -5,22 +5,21 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
-import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val count_button = findViewById<Button>(R.id.counter_btn)
-        val count_txt = findViewById<TextView>(R.id.counter_txt)
-        var counter_var = 0;
+        val countbutton = findViewById<Button>(R.id.counter_btn)
+        val counttxt = findViewById<TextView>(R.id.counter_txt)
+        var countervar = 0
 
-        count_button.setOnClickListener {
-            counter_var = counter_var + 1
-            count_txt.text = "You clicked : " + counter_var + " times"
-            if (counter_var == 10) {
-                counter_var = 0
+        countbutton.setOnClickListener {
+            countervar += 1
+            counttxt.text = "You clicked : " + countervar + " times"
+            if (countervar == 10) {
+                countervar = 0
                 Toast.makeText(this, "Hey You reached the Limit!", Toast.LENGTH_LONG).show()
             }
         }
