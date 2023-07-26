0c2fff1f339af7f2649f5850f78c3c0523fe3b9d
Sasidharan A <rajiisasi2004@gmail.com>
2023-07-26 21:01:52 +0530

    added qr scanner
    Zxing library
    minimum api level : 24
    support on landscape

--- app/src/main/java/in/co/learn/MainActivity.kt
+++ app/src/main/java/in/co/learn/MainActivity.kt
@@ -1,27 +1,49 @@
 package `in`.co.learn
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
-
+import androidx.appcompat.app.AppCompatActivity
+import com.google.zxing.integration.android.IntentIntegrator
 class MainActivity : AppCompatActivity() {
+    lateinit var btnBarcode: Button
+    lateinit var textView: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        title = "KotlinApp"
+        btnBarcode = findViewById(R.id.button)
+        textView = findViewById(R.id.txtContent)
+        btnBarcode.setOnClickListener {
+            val intentIntegrator = IntentIntegrator(this@MainActivity)
+            intentIntegrator.setBeepEnabled(false)
+            intentIntegrator.setCameraId(0)
+            intentIntegrator.setPrompt("SCAN")
+            intentIntegrator.setBarcodeImageEnabled(false)
+            intentIntegrator.initiateScan()
+        }
+    }
+    override fun onActivityResult(
+        requestCode: Int,
+        resultCode: Int,
+        data: Intent?
+    ) {
+        val result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
+        if (result != null) {
+            if (result.contents == null) {
+                Toast.makeText(this, "cancelled", Toast.LENGTH_SHORT).show()
+            } else {
+                Log.d("MainActivity", "Scanned")
+                Toast.makeText(this, "Scanned -> " + result.contents, Toast.LENGTH_SHORT)
+                    .show()
+                textView.text = String.format("Scanned Result: %s", result)
 
-        val countbutton = findViewById<Button>(R.id.counter_btn)
-        val counttxt = findViewById<TextView>(R.id.counter_txt)
-        var countervar = 0
-
-        countbutton.setOnClickListener {
-            countervar += 1
-            counttxt.text = "You clicked : " + countervar + " times"
-            if (countervar == 10) {
-                countervar = 0
-                Toast.makeText(this, "Hey You reached the Limit!", Toast.LENGTH_LONG).show()
             }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data)
         }
     }
 }
